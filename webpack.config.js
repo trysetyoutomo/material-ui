120eaf53290099b339e1cbacc3cb6f0ee614aab7
tsu <try35u@gmail.com>
2022-05-19 07:03:40 +0700

    update practice
    learn from https://material.io/develop/web/getting-started

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const autoprefixer = require('autoprefixer');
-module.exports = [{
-  entry: './app.scss',
+
+module.exports = {
+  entry: ['./app.scss', './app.js'],
   output: {
-    // This is necessary for webpack to compile
-    // But we never use style-bundle.js
-    filename: 'style-bundle.js',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    inline: false
   },
   module: {
     rules: [
@@ -19,16 +21,16 @@ module.exports = [{
           },
           { loader: 'extract-loader' },
           { loader: 'css-loader' },
-           {
-                loader: 'postcss-loader',
-                options: {
-                    postcssOptions: {
-                        plugins: [
-                            autoprefixer()
-                        ]
-                    }
-                }
-            },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer()
+                ]
+              }
+            }
+          },
           {
             loader: 'sass-loader',
             options: {
@@ -36,14 +38,21 @@ module.exports = [{
               implementation: require('sass'),
 
               // See https://github.com/webpack-contrib/sass-loader/issues/804
-            webpackImporter: false,
-            sassOptions: {
-                includePaths: ['./node_modules']
+              webpackImporter: false,
+              sassOptions: {
+                includePaths: ['./node_modules'],
+              },
             },
-            },
-          },
-        ]
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // query: {
+        //   presets: ['@babel/preset-env'],
+        // },
       }
-    ]
+    ],
   },
-}];
+};
